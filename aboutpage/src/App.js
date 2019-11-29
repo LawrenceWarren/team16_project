dbeecbbb6f1aaf37a0ba661f9f0409e36686a5b9
Lawrence Warren <psyljw@nottingham.ac.uk>
2019-11-29 15:49:01 +0000

    Added comments which detail what I want from the app - changed the app slightly, for practice

--- aboutpage/src/App.js
+++ aboutpage/src/App.js
@@ -2,22 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() 
-{
+//PROTOTYPE:
+//Have 3 or 4 JSON with information about restaurants / facilities
+// - Pictures (1 or 2)
+// - Name
+// - Pricing (Cheap/medium/expensive)
+// - Discount available? (Y/N)
+// - Distance (walking time)
+// - Location (Address)
+//Make the react app which can read these JSON and display them in a SPA
+// - Arrows to switch between restaurants
+//   - Smooth transitions - slides across
+// - Formats page so they have consistent styling
+// Don't merge this into master!
+// - Perhaps make a "prototypes" branch which we can push to.
+
+function App() {
   return (
-    <div className="App">
+    <div className="AboutPage">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> New test? </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://projects.cs.nott.ac.uk/COMP2002/2019-2020/team16_project"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitLab!
         </a>
       </header>
     </div>
