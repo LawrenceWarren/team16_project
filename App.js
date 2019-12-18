c2b22f13748d3891762c3c9961ed8a769ba5f40f
psyas11 <psyas11@exmail.nottingham.ac.uk>
2019-12-18 10:25:04 +0000

    Upload New File

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import About from './About';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Ava's Angels</h1>
+    <About />
+    </div>
+  );
+}
+
+export default App;
