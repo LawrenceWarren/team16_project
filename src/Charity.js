c5c05bc7c1dc1c3277365bf7d8b55a4de560804a
psybjsm <psybjsm@exmail.nottingham.ac.uk>
2020-02-13 14:05:53 +0000

    Hardcoded Charity Info

--- src/Charity.js
+++ src/Charity.js
@@ -11,6 +11,25 @@ class Charity extends React.Component {
 
                 <div> This is the Charity page </div>
 
+                <div> Here are a list of the charities near Birmingham children's hospital:
+                <ul>
+                    <li>Birmingham Mind</li>
+                    <li>Breast Cancer Support Charity</li> 
+                    <li>Baron Davenport's Charity</li> 
+                    <li>Cancer Research UK</li> 
+                    <li>Birmingham Sports and Education Foundation</li> 
+                    <li>Heart Research UK Midlands</li> 
+                    <li>CLDF - Children's Liver Disease Charity</li> 
+                    <li>British Heart Foundation Furniture and Electrical</li> 
+                    <li>The Prince's Trust Birmingham Centre</li> 
+                    <li>Personal Support Unit</li>   
+                    <li>Birmingham LGBT</li> 
+                    <li>RSPB</li> 
+                    <li>Smart Works Birmingham</li>   
+                </ul>
+                
+                </div>
+
                 <Footer />
             </div>
             )
