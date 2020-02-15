5c7a6f8c435fec24e5b4faf1817bf04973214c56
Lawrence Warren <lawrencewarren2@gmail.com>
2020-02-15 15:06:13 +0000

    began work on the food page proper

--- src/Food.js
+++ src/Food.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import './css/Food.css';
-import Header from './Header';
+import React from "react";
+import "./css/Food.css";
+import Header from "./Header";
 import Footer from "./Footer";
 
 class Food extends React.Component {
-    render(){
-        return(
-            <div>
-                <Header />
+  render() {
+    return (
+      <div>
+        <Header />
+        <br />
+        <br />
 
-                <div> This is the Food page. Welcome!</div>
+        <div> This is the Food page. Welcome!</div>
 
-                <Footer />
-            </div>
-            )
-        }
-    }
+        <Footer />
+      </div>
+    );
+  }
+}
+
+var thePic =
+  "https://www.bunkwings.com/wp-content/uploads/2015/07/bunk-cocktails-and-wings-nottingham172.jpg";
+
+function setPicture(val) {
+  {
+    thePic = val;
+  }
+}
+
+function leftSwipe() {
+  //document.getElementById('picture').src=
+  //"https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg";
+  setPicture(
+    "https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg"
+  );
+}
+
+function rightSwipe() {
+  //document.getElementById("picture").src=
+  //"https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg";
+  setPicture(
+    "https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg"
+  );
+}
 
 export default Food;
