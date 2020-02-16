ffbb832c3241f14bd29af0b3db66f690abad8e39
Lawrence Warren <psyljw@nottingham.ac.uk>
2020-02-16 11:45:32 +0000

    working on button functionality

--- src/Food.js
+++ src/Food.js
@@ -6,29 +6,29 @@ import Footer from "./Footer";
 var thePic =
   "https://www.bunkwings.com/wp-content/uploads/2015/07/bunk-cocktails-and-wings-nottingham172.jpg";
 
-function setPicture(val) {
-  {
-    thePic = val;
-  }
-}
-
-function leftSwipe() {
-  //document.getElementById('picture').src=
-  //"https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg";
-  setPicture(
-    "https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg"
-  );
-}
-
-function rightSwipe() {
-  //document.getElementById("picture").src=
-  //"https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg";
-  setPicture(
-    "https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg"
-  );
-}
-
 class Food extends React.Component {
+  setPicture = val => {
+    {
+      thePic = val;
+    }
+  };
+
+  leftSwipe = () => {
+    //document.getElementById('picture').src=
+    //"https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg";
+    setPicture(
+      "https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg"
+    );
+  };
+
+  rightSwipe = () => {
+    //document.getElementById("picture").src=
+    //"https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg";
+    setPicture(
+      "https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg"
+    );
+  };
+
   render() {
     return (
       <div>
@@ -37,13 +37,13 @@ class Food extends React.Component {
         <br />
 
         <div class="buttonrow">
-          <button class="left-button" onclick={rightSwipe()}>
+          <button class="left-button" onclick={this.rightSwipe()}>
             {"<"}
           </button>
 
           <img id="picture" src={thePic} class="picture" />
 
-          <button class="right-button" onclick={leftSwipe()}>
+          <button class="right-button" onclick={this.leftSwipe()}>
             {">"}
           </button>
         </div>
