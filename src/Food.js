691458b9bee687cb89cc839054d81719f1488679
Lawrence Warren <lawrencewarren2@gmail.com>
2020-02-16 02:27:05 +0000

    added content; centred content

--- src/Food.js
+++ src/Food.js
@@ -3,22 +3,6 @@ import "./css/Food.css";
 import Header from "./Header";
 import Footer from "./Footer";
 
-class Food extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <br />
-        <br />
-
-        <div> This is the Food page. Welcome!</div>
-
-        <Footer />
-      </div>
-    );
-  }
-}
-
 var thePic =
   "https://www.bunkwings.com/wp-content/uploads/2015/07/bunk-cocktails-and-wings-nottingham172.jpg";
 
@@ -42,6 +26,38 @@ function rightSwipe() {
   setPicture(
     "https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg"
   );
+}
+
+class Food extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <br />
+        <br />
+
+      <div class="buttonrow">
+        <button class="left-button" onclick={rightSwipe()}>
+          {"<"}
+        </button>
+
+        <img id="picture" src={thePic} class="picture" />
+
+        <button class="right-button" onclick={leftSwipe()}>
+          {">"}
+        </button>
+      </div>
+
+        <p class="content">content 1</p>
+        <p class="content">content 2</p>
+        <p class="content">content 3</p>
+        <p class="content">content 4</p>
+        <p class="content">content 5</p>
+
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Food;
