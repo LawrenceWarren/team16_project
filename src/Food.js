247f72cb621ede550b92a15e317f2361be01d66f
Lawrence Warren <psyljw@nottingham.ac.uk>
2020-02-17 20:54:39 +0000

    made content change on button presses

--- src/Food.js
+++ src/Food.js
@@ -10,18 +10,46 @@ class Food extends React.Component {
     this.leftSwipe = this.onClickBack.bind(this);
     this.rightSwipe = this.onClickForward.bind(this);
 
-    const img0 = require("./resource/placeholder0.png");
-    const img1 = require("./resource/placeholder1.png");
-    const img2 = require("./resource/placeholder2.png");
+    const food0 =
+    {
+      image: require("./resource/placeholder0.png"),
+      name: "Example 0",
+      address: "Near hospital 0, BI0 000",
+      type: "cafe 0",
+      price: "£example 0",
+      link: "example.com"
+    };
+
+    const food1 =
+    {
+      image: require("./resource/placeholder1.png"),
+      name: "Example 1",
+      address: "Near hospital 1, BI1 111",
+      type: "cafe 1",
+      price: "£example 1",
+      link: "example.com"
+    };
+
+    const food2 =
+    {
+      image: require("./resource/placeholder2.png"),
+      name: "Example 2",
+      address: "Near hospital 2, BI2 222",
+      type: "cafe 2",
+      price: "£example 2",
+      link: "example.com"
+    };
+
+
 
     this.state = {
       index: 1,
-      imgList: [img0, img1, img2]
+      foodList: [food0, food1, food2]
     };
   }
 
   onClickForward = () => {
-    if (this.state.index + 1 === this.state.imgList.length) {
+    if (this.state.index + 1 === this.state.foodList.length) {
       this.setState({
         index: 0
       });
@@ -35,7 +63,7 @@ class Food extends React.Component {
   onClickBack = () => {
     if (this.state.index - 1 === -1) {
       this.setState({
-        index: this.state.imgList.length - 1
+        index: this.state.foodList.length - 1
       });
     } else {
       this.setState({
@@ -57,7 +85,7 @@ class Food extends React.Component {
           </button>
 
           <img
-            src={this.state.imgList[this.state.index]}
+            src={this.state.foodList[this.state.index].image}
             class="picture"
             alt=""
           />
@@ -65,13 +93,28 @@ class Food extends React.Component {
           <button class="imgButton" onClick={this.onClickForward}>
             {"=>"}
           </button>
-        </div>
 
-        <p class="content">content 1</p>
-        <p class="content">content 2</p>
-        <p class="content">content 3</p>
-        <p class="content">content 4</p>
-        <p class="content">content 5</p>
+          <b>
+            <p class="title">Name</p>
+          </b>
+          <p class="content">{this.state.foodList[this.state.index].name}</p>
+          <b>
+            <p class="title">Address</p>
+          </b>
+          <p class="content">{this.state.foodList[this.state.index].address}</p>
+          <b>
+            <p class="title">Type</p>
+          </b>
+          <p class="content">{this.state.foodList[this.state.index].type}</p>
+          <b>
+            <p class="title">Price</p>
+          </b>
+          <p class="content">{this.state.foodList[this.state.index].price}</p>
+          <b>
+            <p class="title">External Link</p>
+          </b>
+          <a href={this.state.foodList[this.state.index].link}><p class="content">{this.state.foodList[this.state.index].link}</p></a>
+        </div>
 
         <Footer />
       </div>
