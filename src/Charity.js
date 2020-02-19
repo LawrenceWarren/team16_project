0d3cb36cb25f75e5e7b13275daab8433b1bc0004
psybjsm <psybjsm@exmail.nottingham.ac.uk>
2020-02-19 10:11:08 +0000

    Changed text

--- src/Charity.js
+++ src/Charity.js
@@ -11,20 +11,21 @@ class Charity extends React.Component {
 
                 <div> 
                     
-                    Here are a list of the charities near Birmingham children\'s hospital: </div>
+                    Here are a list of the charities near Birmingham children's hospital: </div>
                     <div>Birmingham Mind</div>
                     <div>Breast Cancer Support Charity</div> 
-                    <div>Baron Davenport\'s Charity</div> 
+                    <div>Baron Davenport's Charity</div> 
                     <div>Cancer Research UK</div> 
-                    <div>Birmingham Sports and Education Foundation</div> 
+                    <div>Birmingham Sports and Education Foundation</div>
                     <div>Heart Research UK Midlands</div> 
-                    <div>CLDF - Children\'s Liver Disease Charity</div> 
+                    <div>CLDF - Children's Liver Disease Charity</div> 
                     <div>British Heart Foundation Furniture and Electrical</div> 
-                    <div>The Prince\'s Trust Birmingham Centre</div> 
+                    <div>The Prince's Trust Birmingham Centre</div> 
                     <div>Personal Support Unit</div>   
                     <div>Birmingham LGBT</div> 
                     <div>RSPB</div> 
                     <div>Smart Works Birmingham</div>
+
 
                 <Footer />
             </div>
