7d97d286e90fce8b1313caafbe9c3c3111a5cb77
scyyc1 <scyyc1@nottingham.ac.uk>
2020-03-12 11:20:16 +0000

    contact page

---
+++ model/Contact.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Create Schema
+contactInfo = new Schema({
+    firstname:{
+        type: String,
+        required: true
+    },
+    lastname:{
+        type: String,
+        required: true
+    },
+    email:{
+        type:String,
+        required: false
+    },
+    phoneNum:{
+        type: String,
+        required: true
+    },
+    message:{
+        type: String,
+        required: true,
+    }
+});
+
+//Create Model
+//"User" is corresspond to the collection name in database for Users
+const Contact = mongoose.model("contactInfo", contactInfo);
+module.exports = Contact;
