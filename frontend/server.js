d4b96ff566ddd8221295899d191229a9f6790435
Lawrence Warren <lawrencewarren2@gmail.com>
2020-04-09 22:41:05 +0100

    frontend server now

--- frontend/server.js
+++ frontend/server.js
@@ -1,20 +1,21 @@
-import express, { static } from "express";
-import favicon from "express-favicon";
-import { join } from "path";
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
 
 const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(favicon(__dirname + "/build/favicon.ico"));
-app.use(static(__dirname));
-app.use(static(join(__dirname, "build")));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/ping", function (_req, res) {
+app.get("/ping", function (req, res) {
   return res.send("pong");
 });
 
-app.get("/*", function (_req, res) {
-  res.sendFile(join(__dirname, "build", "index.html"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port);
+console.log("App listening on " + port);
