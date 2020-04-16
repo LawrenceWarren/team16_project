d6daed24152c40a52d72181c2194241a6d1ec62f
Lawrence Warren <lawrencewarren2@gmail.com>
2020-04-16 14:52:08 +0100

    Updated formatting and some resources

--- src/Feedback.js
+++ src/Feedback.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './css/Feedback.css';
-import Header from './Header';
+import React from "react";
+import "./css/Feedback.css";
+import Header from "./Header";
 import Footer from "./Footer";
 
 class Feedback extends React.Component {
-    render(){
-        return(
-            <div>
-                <Header />
+  render() {
+    return (
+      <div>
+        <Header />
 
-                <div> This is the Feedback page </div>
+        <div> This is the Feedback page </div>
 
-                <Footer />
-            </div>
-            )
-        }
-    }
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default Feedback;
