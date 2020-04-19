1486e1a6b843879ac839c564460e46fe0275dae0
Lawrence Warren <lawrencewarren2@gmail.com>
2020-04-19 21:07:03 +0100

    changes to the project

--- frontend/src/Charity.js
+++ frontend/src/Charity.js
@@ -1,151 +1,149 @@
-import React from 'react';
-import './css/Charity.css';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import Header from './Header';
+import React from "react";
+import "./css/Charity.css";
+import Header from "./Header";
 import Footer from "./Footer";
-import SearchIcon from "./resource/charitySearch.jpg";
 import { Swipeable } from "react-swipeable";
 
 class Charity extends React.Component {
-    constructor() {
-      super(); //Does something ??
-  
-      this.leftSwipe = this.onClickBack.bind(this); //Binds the onClickBack function on page loading
-      this.rightSwipe = this.onClickForward.bind(this); //Binds the onClickFroward function on page load
-  
-      //State property
-      this.state = {
-        index: 0, //Used for indexing the array
-        charityList: [
-          {
-            charityId: 0,
-            charity_name: "",
-            charity_phone: "",
-            charity_email: "",
-            charity_weblink: "",
-            charity_introduce: "",
-            charity_image: ""
-          },
-        ],
-      };
-    }
-  
-    //Handle swipes forward
-    //Sets the state of index to increase by 1,
-    // or wrap around to 0
-    onClickForward = () => {
-      if (this.state.index + 1 === this.state.charityList.length) {
-        this.setState({
-          index: 0,
-        });
-      } else {
-        this.setState({
-          index: this.state.index + 1,
-        });
-      }
+  constructor() {
+    super(); //Does something ??
+
+    this.leftSwipe = this.onClickBack.bind(this); //Binds the onClickBack function on page loading
+    this.rightSwipe = this.onClickForward.bind(this); //Binds the onClickFroward function on page load
+
+    //State property
+    this.state = {
+      index: 0, //Used for indexing the array
+      charityList: [
+        {
+          charityId: 0,
+          charity_name: "",
+          charity_phone: "",
+          charity_email: "",
+          charity_weblink: "",
+          charity_introduce: "",
+          charity_image: "",
+        },
+      ],
     };
-    //Handles swipe back
-    //Sets the state of index to decrease by 1,
-    //or wrap around to the largest value
-    onClickBack = () => {
-      if (this.state.index - 1 === -1) {
-        this.setState({
-          index: this.state.charityList.length - 1,
-        });
-      } else {
-        this.setState({
-          index: this.state.index - 1,
-        });
-      }
-    };
-  
-    //Calls the server upon page loading
-    callServer() {
-      fetch("http://localhost:4000/charity") //Fetch from the server
-        .then((res) => res.json()) //JSONify the data
-        .then((res) => this.setState({ charityList: res })) //Pass the JSON into state
-        .catch((err) => err);
-    }
-  
-    //Mounts the callServer function upon page loading, calling it
-    componentDidMount() {
-      this.callServer();
-    }
-  
-    //!-------------!//
-    //!Render method!//
-    //!-------------!//
-    render() {
-      //Print for debugging purposes
-      console.log("Debug: " + this.state.charityList[this.state.index]?.charity_name);
-  
-      //!DOM
-      return (
-        <div className="mainDiv">
-          <Header />
-  
-          <p className="banner"> Below are a selection of local charities with links to their webpages </p>
-  
-          {/*Creates a swipeable div element, which allows for touch control
-           * However, swipeable does not allow for styling, hence the nested div*/}
-          <Swipeable
-            onSwipedLeft={this.onClickBack}
-            onSwipedRight={this.onClickForward}
-          >
-            <div className="contentContainer">
-              <img
-                src="https://www.barondavenportscharity.org/sites/all/themes/custom/nestor_subtheme/logo.png"
-                className="picture"
-                alt=""
-              />
-  
-              <b>
-                <p className="title">Name</p>
-              </b>
-              <p className="content">
-                {this.state.charityList[this.state.index]?.charity_name}
-              </p>
-  
-              <b>
-                <p className="title">Phone</p>
-              </b>
-              <p className="content">
-                {this.state.charityList[this.state.index]?.charity_phone}
-              </p>
-  
-              <b>
-                <p className="title">Email</p>
-              </b>
-              <p className="content">
-                {this.state.charityList[this.state.index]?.charity_email}
-              </p>
-              <b>
-                <p className="title">Introduction</p>
-              </b>
-              <p className="content">
-                {this.state.charityList[this.state.index]?.charity_introduce}
-              </p>
-  
-              <b>
-                <p className="title">Weblink</p>
-              </b>
-              <a href={this.state.charityList[this.state.index]?.charity_weblink}>
-                <p className="content">
-                  {this.state.charityList[this.state.index]?.charity_weblink}
-                </p>
-              </a>
-            </div>
-          </Swipeable>
-  
-          <Footer />
-        </div>
-      );
-    }
   }
-  
-  export default Charity;
+
+  //Handle swipes forward
+  //Sets the state of index to increase by 1,
+  // or wrap around to 0
+  onClickForward = () => {
+    if (this.state.index + 1 === this.state.charityList.length) {
+      this.setState({
+        index: 0,
+      });
+    } else {
+      this.setState({
+        index: this.state.index + 1,
+      });
+    }
+  };
+  //Handles swipe back
+  //Sets the state of index to decrease by 1,
+  //or wrap around to the largest value
+  onClickBack = () => {
+    if (this.state.index - 1 === -1) {
+      this.setState({
+        index: this.state.charityList.length - 1,
+      });
+    } else {
+      this.setState({
+        index: this.state.index - 1,
+      });
+    }
+  };
+
+  //Calls the server upon page loading
+  callServer() {
+    fetch("http://localhost:4000/charity") //Fetch from the server
+      .then((res) => res.json()) //JSONify the data
+      .then((res) => this.setState({ charityList: res })) //Pass the JSON into state
+      .catch((err) => err);
+  }
+
+  //Mounts the callServer function upon page loading, calling it
+  componentDidMount() {
+    this.callServer();
+  }
+
+  //!-------------!//
+  //!Render method!//
+  //!-------------!//
+  render() {
+    //Print for debugging purposes
+    console.log(
+      "Debug: " + this.state.charityList[this.state.index]?.charity_name
+    );
+
+    //!DOM
+    return (
+      <div className="mainDiv">
+        <Header />
+
+        <p className="banner">
+          {" "}
+          Below are a selection of local charities with links to their webpages{" "}
+        </p>
+
+        {/*Creates a swipeable div element, which allows for touch control
+         * However, swipeable does not allow for styling, hence the nested div*/}
+        <Swipeable
+          onSwipedLeft={this.onClickBack}
+          onSwipedRight={this.onClickForward}
+        >
+          <div className="contentContainer">
+            <img
+              src="https://www.barondavenportscharity.org/sites/all/themes/custom/nestor_subtheme/logo.png"
+              className="picture"
+              alt=""
+            />
+
+            <b>
+              <p className="title">Name</p>
+            </b>
+            <p className="content">
+              {this.state.charityList[this.state.index]?.charity_name}
+            </p>
+
+            <b>
+              <p className="title">Phone</p>
+            </b>
+            <p className="content">
+              {this.state.charityList[this.state.index]?.charity_phone}
+            </p>
+
+            <b>
+              <p className="title">Email</p>
+            </b>
+            <p className="content">
+              {this.state.charityList[this.state.index]?.charity_email}
+            </p>
+            <b>
+              <p className="title">Introduction</p>
+            </b>
+            <p className="content">
+              {this.state.charityList[this.state.index]?.charity_introduce}
+            </p>
+
+            <b>
+              <p className="title">Weblink</p>
+            </b>
+            <a href={this.state.charityList[this.state.index]?.charity_weblink}>
+              <p className="content">
+                {this.state.charityList[this.state.index]?.charity_weblink}
+              </p>
+            </a>
+          </div>
+        </Swipeable>
+
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Charity;
--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -1,99 +1,114 @@
-import React from 'react';
-import './css/Contact.css';
-import Header from './Header';
+import React from "react";
+import "./css/Contact.css";
+import Header from "./Header";
 import Footer from "./Footer";
 
 class Contact extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            phone: '',
-            message: 'Leave your message here'
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      message: "Leave your message here",
+    };
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleInputChange (event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
 
-        this.setState({
-            [name] : value
-        })
-    }
+    this.setState({
+      [name]: value,
+    });
+  }
 
-    handleSubmit(event) {
-        {/*To connect database */}
-        event.preventDefault();
-    }
+  handleSubmit(event) {
+    //To connect database
+    event.preventDefault();
+  }
 
-    render(){
-        return(
+  render() {
+    return (
+      <div>
+        <Header />
+
+        <div class="contact_header"> Contact </div>
+
+        <div align="center">
+          <form class="contact_form">
             <div>
-                <Header />
-
-                <div class = "contact_header"> Contact </div>
-
-                <div align = "center">
-                    <form class = "contact_form">
-                        <div>
-                            <label><strong>First Name *</strong></label>
-                            <input 
-                                name="firstName" 
-                                type="text" 
-                                value={this.state.firstName}
-                                onChange={this.handleInputChange}
-                                required/>
-                        </div>
-                        <div>
-                            <label><strong>Last Name *</strong></label>
-                            <input 
-                                name="lastName" 
-                                type="text" 
-                                value={this.state.lastName}
-                                onChange={this.handleInputChange}
-                                required/>
-                        </div>
-                        <div>
-                            <label><strong>Email Address</strong></label>
-                            <input 
-                                name="email" 
-                                type="text" 
-                                value={this.state.email}
-                                onChange={this.handleInputChange}
-                                required/>
-                        </div>
-                        <div>
-                            <label><strong>Phone number *</strong></label>
-                            <input 
-                                name="phone" 
-                                type="text" 
-                                value={this.state.phone}
-                                onChange={this.handleInputChange}
-                                required/>
-                        </div>
-                        <div>
-                            <label><strong>Message</strong></label>
-                            <textarea
-                                name="message"
-                                value={this.state.message}
-                                onChange={this.handleInputChange} />
-                        </div>
-                        <button type = "submit">Submit</button>
-                        <button type = "reset">Reset</button>
-                    </form>
-                </div>
-
-                <Footer />               
+              <label>
+                <strong>First Name *</strong>
+              </label>
+              <input
+                name="firstName"
+                type="text"
+                value={this.state.firstName}
+                onChange={this.handleInputChange}
+                required
+              />
             </div>
-            )
-        }
-    }
+            <div>
+              <label>
+                <strong>Last Name *</strong>
+              </label>
+              <input
+                name="lastName"
+                type="text"
+                value={this.state.lastName}
+                onChange={this.handleInputChange}
+                required
+              />
+            </div>
+            <div>
+              <label>
+                <strong>Email Address</strong>
+              </label>
+              <input
+                name="email"
+                type="text"
+                value={this.state.email}
+                onChange={this.handleInputChange}
+                required
+              />
+            </div>
+            <div>
+              <label>
+                <strong>Phone number *</strong>
+              </label>
+              <input
+                name="phone"
+                type="text"
+                value={this.state.phone}
+                onChange={this.handleInputChange}
+                required
+              />
+            </div>
+            <div>
+              <label>
+                <strong>Message</strong>
+              </label>
+              <textarea
+                name="message"
+                value={this.state.message}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <button type="submit">Submit</button>
+            <button type="reset">Reset</button>
+          </form>
+        </div>
+
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default Contact;
