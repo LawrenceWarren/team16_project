1254fb17405bddf30b19c109324c144c3b875634
Lawrence Warren <lawrencewarren2@gmail.com>
2020-06-04 17:06:39 +0100

    changed the table in the contact information admin function to be HTML - i.e. not rely on a NPM dependency

--- src/Admin/functions/AdminCheckContact.js
+++ src/Admin/functions/AdminCheckContact.js
@@ -1,50 +1,16 @@
 //This code was written by Yutian Chen, with small changes by Lawrence Warren.
 
 import React from "react";
-import { Table } from "antd";
-
-const columns = [
-  {
-    title: "First Name",
-    dataIndex: "firstname",
-    key: "firstname",
-  },
-  {
-    title: "Last Name",
-    dataIndex: "lastname",
-    key: "lastname",
-  },
-  {
-    title: "Email Address",
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "Phone number",
-    dataIndex: "phoneNum",
-    key: "phoneNum",
-  },
-  {
-    title: "Message",
-    dataIndex: "message",
-    key: "message",
-  },
-  {
-    title: "Time",
-    dataIndex: "currentTime",
-    key: "currentTime",
-  },
-];
 
 class AdminCheckContact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      Infos: [
+      details: [
         {
           currentTime: "",
-          firstName: "",
-          lastName: "",
+          firstname: "",
+          lastname: "",
           email: "",
           phoneNum: "",
           message: "",
@@ -53,40 +19,90 @@ class AdminCheckContact extends React.Component {
     };
   }
 
-  componentDidMount = () => {
-    this.getContactInfo();
-  };
+  componentDidMount = async () => {
+    //Fetching from server
+    console.log("ContactCMS: Fetching from server...");
+    try {
+      const res = await fetch("/contactReq");
+      if (res.status >= 400) {
+        throw new Error("There was an error in the HTTP request.");
+      }
 
-  getContactInfo = () => {
-    fetch("/contactReq") //Fetch from the server
-      .then((res) => res.json()) //JSONify the data
-      .then((res) => this.setState({ Infos: res })) //Pass the JSON into state
-      .catch((err) => err);
-  };
-
-  // Switch the date information to readable form
-  switchDate(milliseconds) {
-    var date = new Date(milliseconds);
-    return date.toUTCString();
-  }
-
-  render() {
-    // Switch the date information to readable form
-    const newInfo = this.state.Infos;
-    for (var i = 0; i < newInfo.length; i++) {
-      newInfo[i].currentTime = this.switchDate(Number(newInfo[i].currentTime));
+      const info = await res.json();
+      this.setState({ details: info });
+      console.log("ContactCMS: Data from the server has been received!");
+    } catch (err) {
+      console.error("ContactCMS: " + err);
     }
 
+    //Putting into table
+
+    var cell0 = document.createElement("td");
+    var cell1 = document.createElement("td");
+    var cell2 = document.createElement("td");
+    var cell3 = document.createElement("td");
+    var cell4 = document.createElement("td");
+    var cell5 = document.createElement("td");
+    var row = document.createElement("tr");
+    document.getElementById("contactInfo").appendChild(row);
+
+    //Adding the firstname title
+    cell0.appendChild(document.createTextNode("First Name"));
+    row.appendChild(cell0);
+    //Adding the lastname title
+    cell1.appendChild(document.createTextNode("Last Name"));
+    row.appendChild(cell1);
+    //Adding the email title
+    cell2.appendChild(document.createTextNode("Email Address"));
+    row.appendChild(cell2);
+    //adding the phone number title
+    cell3.appendChild(document.createTextNode("Phone Number"));
+    row.appendChild(cell3);
+    //adding the message title
+    cell4.appendChild(document.createTextNode("Message"));
+    row.appendChild(cell4);
+    //adding the delete button title
+    cell5.appendChild(document.createTextNode("Delete"));
+    row.appendChild(cell5);
+
+    //For each element in the details
+    this.state.details.forEach((contact) => {
+      console.log(contact);
+
+      cell0 = document.createElement("td");
+      cell1 = document.createElement("td");
+      cell2 = document.createElement("td");
+      cell3 = document.createElement("td");
+      cell4 = document.createElement("td");
+      cell5 = document.createElement("td");
+      row = document.createElement("tr");
+      document.getElementById("contactInfo").appendChild(row);
+
+      //Adding the firstname
+      cell0.appendChild(document.createTextNode(contact.firstname));
+      row.appendChild(cell0);
+      //Adding the lastname
+      cell1.appendChild(document.createTextNode(contact.lastname));
+      row.appendChild(cell1);
+      //Adding the email
+      cell2.appendChild(document.createTextNode(contact.email));
+      row.appendChild(cell2);
+      //adding the phone number
+      cell3.appendChild(document.createTextNode(contact.phoneNum));
+      row.appendChild(cell3);
+      //adding the message
+      cell4.appendChild(document.createTextNode(contact.message));
+      row.appendChild(cell4);
+      //adding the delete button
+      //TODO: add a delete button to the table
+    });
+  };
+
+  render() {
     return (
       <div>
         <h1>Contact Information</h1>
-        <div>
-          <Table
-            dataSource={this.state.Infos}
-            columns={columns}
-            rowKey="currentTime"
-          />
-        </div>
+        <table id="contactInfo" border="1"></table>
       </div>
     );
   }
